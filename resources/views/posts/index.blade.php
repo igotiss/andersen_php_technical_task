4c9bdfee8bd5c38a5b2773b78f23ae4103513afa
igotiss <igotiss@gmail.com>
2022-11-02 11:08:33 +0200

    create Posts model, migration, controller, view, routes

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Fill the form to submit message, please') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('posts.store') }}">
+                            @csrf
+
+                            <div class="row mb-3">
+                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="message" class="col-md-4 col-form-label text-md-end">{{ __('Message') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="message" type="text" class="form-control @error('email') is-invalid @enderror" name="message" value="{{ old('message') }}" required autocomplete="message">
+
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+
+
+                            <div class="row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Submit') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <hr class="col-md-8 mt-3">
+            <div class="col-md-8 mt-3">
+
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Message</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($posts as $post)
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>{{$post->name}}</td>
+                        <td>{{$post->email}}</td>
+                        <td>{{$post->message}}</td>
+                    </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Posts
+
+Route
+    ::name('posts.')
+    ->group(function () {
+        Route::get('/', [App\Http\Controllers\PostController::class, 'index'])->name('index');
+        Route::post('/', [App\Http\Controllers\PostController::class, 'store'])->name('store');
+});
+
+
